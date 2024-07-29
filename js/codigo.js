69d7ffa8062f4f6e6669a5fe787a0e53d0556a42
MatiasEzequiel12 <matias_palaciio@hotmail.com>
2024-07-29 12:06:47 -0300

    agregado @media responsive agregado @keyframes agregado Js

--- js/codigo.js
+++ js/codigo.js
@@ -1,28 +1,57 @@
-function abrir() {
-  document.getElementById("desplegable").style.display = "flex";
+function abrir(){
+
+
+document.getElementById('desplegar').style.display = "flex"
+
+
 }
 
-function cerrar() {
-  document.getElementById("desplegable").style.display = "none";
-}
 
-function abrirModal() {
-  document.getElementById("modal").style.display = "flex";
-}
+function cerrar(){
 
-function cerrarModal() {
-  document.getElementById("modal").style.display = "none";
-}
 
-window.addEventListener("scroll", function () {
-  let animacion = document.querySelectorAll(".animado");
-
-  for (let i = 0; i < animacion.length; i++) {
-    let posicionObj1 = animacion[i].getBoundingClientRect().top;
-
-    let tamañoPantalla = window.innerHeight / 3;
-    if (posicionObj1 < tamañoPantalla) {
-      animacion[i].style.animation = "mover 1s ease-out";
+    document.getElementById('desplegar').style.display = "none"
+    
+    
     }
-  }
-});
+
+
+    function abrirModal(){
+
+
+        document.getElementById('modal').style.display = "flex"
+        
+        
+        }
+        
+        
+        function cerrarModal(){
+        
+        
+            document.getElementById('modal').style.display = "none"
+            
+            
+            }
+           
+
+
+window.addEventListener('scroll', function(){
+
+let animacion = document.querySelectorAll(".animado");
+
+for (let i = 0; i < animacion.length; i++) {
+    let posicionObj1 =  animacion[i].getBoundingClientRect().top;
+
+    let tamañoPantalla = window.innerHeight/3;
+    if (posicionObj1 < tamañoPantalla){
+    
+    
+        animacion[i].style.animation= 'mover 2s ease-out';
+    }
+    
+}
+
+
+
+
+    })
